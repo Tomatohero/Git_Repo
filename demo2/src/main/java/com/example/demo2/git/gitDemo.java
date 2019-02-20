8032b09faed0b5c37f3c4772f3d9e1a46f3f9bdc
tomato <2457817475@qq.com>
2019-02-20 15:36:59 +0800

    ojbk!!!

---
+++ demo2/src/main/java/com/example/demo2/git/gitDemo.java
@@ -0,0 +1,25 @@
+package com.example.demo2.git;
+
+public class gitDemo {
+
+    private String username;
+
+    private String userpassword;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getUserpassword() {
+        return userpassword;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setUserpassword(String userpassword) {
+        this.userpassword = userpassword;
+    }
+
+}
